3cf4dd3fa183f14cce381ae257860a61d8af689c
Henry Solberg <milkshakeiii@gmail.com>
2019-01-23 20:30:19 -0800

    general strategy for multidots, works with bfs and dfs

--- pacman/search.py
+++ pacman/search.py
@@ -22,6 +22,8 @@ files and classes when code is run, so be careful to not modify anything else.
 # maze is a Maze object based on the maze from the file specified by input filename
 # searchMethod is the search method specified by --method flag (bfs,dfs,greedy,astar)
 
+import copy
+
 def search(maze, searchMethod):
     return {
         "bfs": bfs,
@@ -30,17 +32,59 @@ def search(maze, searchMethod):
         "astar": astar,
     }.get(searchMethod)(maze)
 
+def general_pacman_search(strategy, maze):
+    nodes_explored = 0
+    start = maze.getStart()
+    frontier = [ ([(start[0], start[1])], set()) ]
+    objectives = maze.getObjectives()
+    forbidden_states = []
+    
+    while len(frontier) > 0:
+        exploring_path = frontier.pop(0)
+        nodes_explored += 1
+        
+        if len(exploring_path[1]) == len(objectives):
+            return_path = [(step[0], step[1]) for step in exploring_path[0]]
+            return return_path, nodes_explored
+        
+        new_neighbors = maze.getNeighbors(exploring_path[0][-1][0], exploring_path[0][-1][1])
+        neighbor_states = []
+        for neighbor in new_neighbors:
+            captured_objectives = copy.deepcopy(exploring_path[1])
+            if neighbor in objectives:
+                captured_objectives.add(neighbor)
+            neighbor_states.append((neighbor, captured_objectives))
+                
+        allowable_neighbor_states = [neighbor for neighbor in neighbor_states if neighbor not in forbidden_states]
+        forbidden_states = forbidden_states + allowable_neighbor_states
+        frontier = strategy(allowable_neighbor_states, exploring_path, frontier, maze)
+
+    raise Exception("No route found")
+
+
+
+def bfs_strategy(append_us, exploring_path, frontier, maze):
+    for state in append_us:
+        frontier = frontier + [(exploring_path[0] + [state[0]], state[1])]
+    return frontier
+
+def dfs_strategy(append_us, exploring_path, frontier, maze):
+    for state in append_us:
+        frontier = [(exploring_path[0] + [state[0]], state[1])] + frontier
+    return frontier
+
+    
 
 def bfs(maze):
-    # TODO: Write your code here
+    
     # return path, num_states_explored
-    return [], 0
+    return general_pacman_search(bfs_strategy, maze)
 
 
 def dfs(maze):
     # TODO: Write your code here
     # return path, num_states_explored
-    return [], 0
+    return general_pacman_search(dfs_strategy, maze)
 
 
 def greedy(maze):
