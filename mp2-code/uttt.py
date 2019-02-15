0ce8281a7292b917c9414a3314b8f1c2f7698a19
Henry Solberg <milkshakeiii@gmail.com>
2019-02-15 13:43:34 -0800

    minimax ez

--- mp2-code/uttt.py
+++ mp2-code/uttt.py
@@ -48,8 +48,7 @@ class ultimateTicTacToe:
         print('\n'.join([' '.join([str(cell) for cell in row]) for row in self.board[3:6]])+'\n')
         print('\n'.join([' '.join([str(cell) for cell in row]) for row in self.board[6:9]])+'\n')
 
-
-    def evaluatePredifined(self, isMax):
+    def evaluatePredefined(self, isMax):
         """
         This function implements the evaluation function for ultimate tic tac toe for predifined agent.
         input args:
@@ -69,15 +68,162 @@ class ultimateTicTacToe:
             if self.count_3_in_a_row(self.minPlayer) > 0:
                 return self.winnerMinUtility
             elif self.count_2_in_a_row(self.minPlayer) > 0 or self.count_preventions(self.minPlayer, self.maxPlayer) > 0:
-                return self.twoInARowMinUtility*self.count_2_in_a_row(self.minPlayer) + self.preventThreeInARowMinUtility*self.count_prevensions(self.minPlayer, self.maxPlayer)
+                return self.twoInARowMinUtility*self.count_2_in_a_row(self.minPlayer) + self.preventThreeInARowMinUtility*self.count_preventions(self.minPlayer, self.maxPlayer)
             else:
                 return self.cornerMinUtility*self.count_corners(self.minPlayer)
-            
-        
+
+    def evaluateDesigned(self, isMax):
+        """
+        This function implements the evaluation function for ultimate tic tac toe for your own agent.
+        input args:
+        isMax(bool): boolean variable indicates whether it's maxPlayer or minPlayer.
+                     True for maxPlayer, False for minPlayer
+        output:
+        score(float): estimated utility score for maxPlayer or minPlayer
+        """
+        #YOUR CODE HERE
         score=0
         return score
 
+    def checkMovesLeft(self):
+        """
+        This function checks whether any legal move remains on the board.
+        output:
+        movesLeft(bool): boolean variable indicates whether any legal move remains
+                        on the board.
+        """
+        #YOUR CODE HERE
+        for row in self.board:
+            for entry in row:
+                if entry == '_':
+                    return True
+        return False
 
+    def checkWinner(self):
+        #Return termimnal node status for maximizer player 1-win,0-tie,-1-lose
+        """
+        This function checks whether there is a winner on the board.
+        output:
+        winner(int): Return 0 if there is no winner.
+                     Return 1 if maxPlayer is the winner.
+                     Return -1 if miniPlayer is the winner.
+        """
+        #YOUR CODE HERE
+        if self.count_3_in_a_row(self.maxPlayer) > 0:
+            return 1
+        if self.count_3_in_a_row(self.minPlayer) > 0:
+            return -1
+        return 0
+
+    def alphabeta(self,depth,currBoardIdx,alpha,beta,isMax):
+        """
+        This function implements alpha-beta algorithm for ultimate tic-tac-toe game.
+        input args:
+        depth(int): current depth level
+        currBoardIdx(int): current local board index
+        alpha(float): alpha value
+        beta(float): beta value
+        isMax(bool):boolean variable indicates whether it's maxPlayer or minPlayer.
+                     True for maxPlayer, False for minPlayer
+        output:
+        bestValue(float):the bestValue that current player may have
+        """
+        #YOUR CODE HERE
+        bestValue=0.0
+        return bestValue
+
+    #based off pseudocode from wikipedia
+    def minimax(self, depth, currBoardIdx, isMax):
+        """
+        This function implements minimax algorithm for ultimate tic-tac-toe game.
+        input args:
+        depth(int): current depth level
+        currBoardIdx(int): current local board index
+        alpha(float): alpha value
+        beta(float): beta value
+        isMax(bool):boolean variable indicates whether it's maxPlayer or minPlayer.
+                     True for maxPlayer, False for minPlayer
+        output:
+        bestValue(float):the bestValue that current player may have
+        """
+        if depth == 0 or not self.checkMovesLeft or self.checkWinner() != 0:
+            return self.evaluatePredefined(isMax)
+
+        if isMax:
+            value = float('-inf')
+            for i in range(len(self.board[currBoardIdx])):
+                marker = self.board[currBoardIdx][i]
+                if marker == '_':
+                    self.board[currBoardIdx][i] = self.maxPlayer
+                    value = max(value, self.minimax(depth-1, i, False))
+                    self.board[currBoardIdx][i] = '_'
+            return value
+        else:
+            value = float('inf')
+            for i in range(len(self.board[currBoardIdx])):
+                marker = self.board[currBoardIdx][i]
+                if marker == '_':
+                    self.board[currBoardIdx][i] = self.minPlayer
+                    value = min(value, self.minimax(depth-1, i, True))
+                    self.board[currBoardIdx][i] = '_'
+            return value
+
+    def playGamePredifinedAgent(self,maxFirst,isMinimaxOffensive,isMinimaxDefensive):
+        """
+        This function implements the processes of the game of predifined offensive agent vs defensive agent.
+        input args:
+        maxFirst(bool): boolean variable indicates whether maxPlayer or minPlayer plays first.
+                        True for maxPlayer plays first, and False for minPlayer plays first.
+        isMinimaxOffensive(bool):boolean variable indicates whether it's using minimax or alpha-beta pruning algorithm for offensive agent.
+                        True is minimax and False is alpha-beta.
+        isMinimaxOffensive(bool):boolean variable indicates whether it's using minimax or alpha-beta pruning algorithm for defensive agent.
+                        True is minimax and False is alpha-beta.
+        output:
+        bestMove(list of tuple): list of bestMove coordinates at each step
+        bestValue(list of float): list of bestValue at each move
+        expandedNodes(list of int): list of expanded nodes at each move
+        gameBoards(list of 2d lists): list of game board positions at each move
+        winner(int): 1 for maxPlayer is the winner, -1 for minPlayer is the winner, and 0 for tie.
+        """
+        #YOUR CODE HERE
+        bestMove=[]
+        bestValue=[]
+        gameBoards=[]
+        winner=0
+        expandedNodes=0
+        return gameBoards, bestMove, expandedNodes, bestValue, winner
+
+    def playGameYourAgent(self):
+        """
+        This function implements the processes of the game of your own agent vs predifined offensive agent.
+        input args:
+        output:
+        bestMove(list of tuple): list of bestMove coordinates at each step
+        gameBoards(list of 2d lists): list of game board positions at each move
+        winner(int): 1 for maxPlayer is the winner, -1 for minPlayer is the winner, and 0 for tie.
+        """
+        #YOUR CODE HERE
+        bestMove=[]
+        gameBoards=[]
+        winner=0
+        return gameBoards, bestMove, winner
+
+
+    def playGameHuman(self):
+        """
+        This function implements the processes of the game of your own agent vs a human.
+        output:
+        bestMove(list of tuple): list of bestMove coordinates at each step
+        gameBoards(list of 2d lists): list of game board positions at each move
+        winner(int): 1 for maxPlayer is the winner, -1 for minPlayer is the winner, and 0 for tie.
+        """
+        #YOUR CODE HERE
+        bestMove=[]
+        gameBoards=[]
+        winner=0
+        return gameBoards, bestMove, winner
+
+        
     def count_corners(self, player_marker):
         count = 0
         for local_board in self.board:
@@ -268,137 +414,6 @@ class ultimateTicTacToe:
                 count += 1
 
         return count
-
-    
-
-    def evaluateDesigned(self, isMax):
-        """
-        This function implements the evaluation function for ultimate tic tac toe for your own agent.
-        input args:
-        isMax(bool): boolean variable indicates whether it's maxPlayer or minPlayer.
-                     True for maxPlayer, False for minPlayer
-        output:
-        score(float): estimated utility score for maxPlayer or minPlayer
-        """
-        #YOUR CODE HERE
-        score=0
-        return score
-
-    def checkMovesLeft(self):
-        """
-        This function checks whether any legal move remains on the board.
-        output:
-        movesLeft(bool): boolean variable indicates whether any legal move remains
-                        on the board.
-        """
-        #YOUR CODE HERE
-        for row in self.board:
-            for entry in row:
-                if entry == '_':
-                    return True
-        return False
-
-    def checkWinner(self):
-        #Return termimnal node status for maximizer player 1-win,0-tie,-1-lose
-        """
-        This function checks whether there is a winner on the board.
-        output:
-        winner(int): Return 0 if there is no winner.
-                     Return 1 if maxPlayer is the winner.
-                     Return -1 if miniPlayer is the winner.
-        """
-        #YOUR CODE HERE
-        winner=0
-        return 0
-
-    def alphabeta(self,depth,currBoardIdx,alpha,beta,isMax):
-        """
-        This function implements alpha-beta algorithm for ultimate tic-tac-toe game.
-        input args:
-        depth(int): current depth level
-        currBoardIdx(int): current local board index
-        alpha(float): alpha value
-        beta(float): beta value
-        isMax(bool):boolean variable indicates whether it's maxPlayer or minPlayer.
-                     True for maxPlayer, False for minPlayer
-        output:
-        bestValue(float):the bestValue that current player may have
-        """
-        #YOUR CODE HERE
-        bestValue=0.0
-        return bestValue
-
-    def minimax(self, depth, currBoardIdx, isMax):
-        """
-        This function implements minimax algorithm for ultimate tic-tac-toe game.
-        input args:
-        depth(int): current depth level
-        currBoardIdx(int): current local board index
-        alpha(float): alpha value
-        beta(float): beta value
-        isMax(bool):boolean variable indicates whether it's maxPlayer or minPlayer.
-                     True for maxPlayer, False for minPlayer
-        output:
-        bestValue(float):the bestValue that current player may have
-        """
-        #YOUR CODE HERE
-        bestValue=0.0
-        return bestValue
-
-    def playGamePredifinedAgent(self,maxFirst,isMinimaxOffensive,isMinimaxDefensive):
-        """
-        This function implements the processes of the game of predifined offensive agent vs defensive agent.
-        input args:
-        maxFirst(bool): boolean variable indicates whether maxPlayer or minPlayer plays first.
-                        True for maxPlayer plays first, and False for minPlayer plays first.
-        isMinimaxOffensive(bool):boolean variable indicates whether it's using minimax or alpha-beta pruning algorithm for offensive agent.
-                        True is minimax and False is alpha-beta.
-        isMinimaxOffensive(bool):boolean variable indicates whether it's using minimax or alpha-beta pruning algorithm for defensive agent.
-                        True is minimax and False is alpha-beta.
-        output:
-        bestMove(list of tuple): list of bestMove coordinates at each step
-        bestValue(list of float): list of bestValue at each move
-        expandedNodes(list of int): list of expanded nodes at each move
-        gameBoards(list of 2d lists): list of game board positions at each move
-        winner(int): 1 for maxPlayer is the winner, -1 for minPlayer is the winner, and 0 for tie.
-        """
-        #YOUR CODE HERE
-        bestMove=[]
-        bestValue=[]
-        gameBoards=[]
-        winner=0
-        expandedNodes=0
-        return gameBoards, bestMove, expandedNodes, bestValue, winner
-
-    def playGameYourAgent(self):
-        """
-        This function implements the processes of the game of your own agent vs predifined offensive agent.
-        input args:
-        output:
-        bestMove(list of tuple): list of bestMove coordinates at each step
-        gameBoards(list of 2d lists): list of game board positions at each move
-        winner(int): 1 for maxPlayer is the winner, -1 for minPlayer is the winner, and 0 for tie.
-        """
-        #YOUR CODE HERE
-        bestMove=[]
-        gameBoards=[]
-        winner=0
-        return gameBoards, bestMove, winner
-
-
-    def playGameHuman(self):
-        """
-        This function implements the processes of the game of your own agent vs a human.
-        output:
-        bestMove(list of tuple): list of bestMove coordinates at each step
-        gameBoards(list of 2d lists): list of game board positions at each move
-        winner(int): 1 for maxPlayer is the winner, -1 for minPlayer is the winner, and 0 for tie.
-        """
-        #YOUR CODE HERE
-        bestMove=[]
-        gameBoards=[]
-        winner=0
-        return gameBoards, bestMove, winner
 
 if __name__=="__main__":
     uttt=ultimateTicTacToe()
