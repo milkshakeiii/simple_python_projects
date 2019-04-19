576674866b74441a5834b3679bc5952aa9635b5e
Henry Solberg <milkshakeiii@gmail.com>
2019-04-18 18:18:35 -0700

    begin part 2

--- skeleton/part 1/agent.py
+++ skeleton/part 1/agent.py
@@ -48,6 +48,7 @@ class Agent:
         (Note that [adjoining_wall_x=0, adjoining_wall_y=0] is also the case when snake runs out of the 480x480 board)
 
         '''
+        ##compute TA's state rep
         snake_head_x, snake_head_y, snake_body, food_x, food_y = state
         adjoining_wall_x = 0
         if snake_head_x == 40:
@@ -81,7 +82,6 @@ class Agent:
             adjoining_body_right = 1
         #print(snake_body, (snake_head_x, snake_head_y), adjoining_body_top, adjoining_body_bottom, adjoining_body_left, adjoining_body_right)
         
-        
         state = (adjoining_wall_x,
                  adjoining_wall_y,
                  food_dir_x,
@@ -100,6 +100,7 @@ class Agent:
                 return_value = u
             return return_value
 
+        #update Q on all but the first turn
         if (self.s != None) and self._train:
             R = -0.1
             if dead:
@@ -111,6 +112,7 @@ class Agent:
             next_action_q = max([self.Q[state + (aprime,)] for aprime in self.actions])
             self.Q[self.s + (self.a,)] = self.Q[self.s + (self.a,)] + alpha*(R + self.gamma*next_action_q - self.Q[self.s + (self.a,)])
 
+        #choose an action and update N if not dead
         if not dead:
             if self._train:
                 possible_actions = [(f(self.Q[state + (aprime,)], self.N[state + (aprime,)]), aprime) for aprime in self.actions]
