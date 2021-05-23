232e5820e209220abb086421f9da73d7e9d62182
Henry Solberg <milkshakeiii@gmail.com>
2021-05-22 21:51:34 -0700

    first attempt at pipelining

--- interview/client.py
+++ interview/client.py
@@ -1,28 +1,83 @@
+import queue
+import select
 import socket
 import sys
+import threading
+
 from collections import deque
 
-HOST, PORT = "localhost", 9999
+HOST = "localhost"
+PORT = 9999
 
-files_found = 0
+prepared_requests = queue.Queue()
+server_responses = queue.Queue()
+directory_frontier = queue.Queue()
+
+def manage_requests():
+    while True:
+        request = prepared_requests.get(block=True)
+        _, ready_to_send, _ = select.select([], [sock], [], 60)
+        if sock in ready_to_send:
+            sock.sendall(request)
+            #print("sent" + str(request))
+
+def manage_responses():
+    unfinished_response = ""
+    while True:
+        ready_to_read, _, _ = select.select([sock], [], [], 60)
+        if sock in ready_to_read:
+            response_data = sock.recv(1024)
+            decoded_response = unfinished_response + str(response_data, "utf-8")
+            split_responses = decoded_response.split(f"END\r\n")
+            unfinished_response = split_responses[-1]
+            for response in split_responses[:-1]:
+                server_responses.put(response)
+
+def manage_exploration():
+    unanswered_dirlists = deque()
+    files_found = 0
+    responses_processed = 0
+
+    def enqueue_request(dir_to_list):
+        unanswered_dirlists.append(dir_to_list)
+        request = bytes(f"DIRLIST " + dir_to_list + f"\r\n", "utf-8")
+        prepared_requests.put(request)
+
+    enqueue_request("/")
+    
+    while True:
+        if not server_responses.empty():
+            received = server_responses.get()
+            responses_processed += 1
+            received_lines = received.split(f"\r\n")
+            parent_dirs = unanswered_dirlists.popleft()
+            if (responses_processed%857==0):
+                print(parent_dirs, len(received_lines), received_lines[-2])
+            for i in range(1, len(received_lines)-1):
+                received_line = received_lines[i].lstrip()
+                if (received_line[-1] == "/"): #we found a directory
+                    dir_to_list = parent_dirs+received_line
+                    enqueue_request(dir_to_list)
+                else: #we found a file
+                    files_found += 1
+
+            #print(responses_processed)
+            if (responses_processed%1000==0):
+                print(responses_processed, len(received_lines), files_found)
+
+    return files_found
+                
+
 # Create a socket (SOCK_STREAM means a TCP socket)
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     sock.connect((HOST, PORT))
+    sock.setblocking(False)
 
-    path_frontier = deque(["/"])
-    while len(path_frontier) > 0:
-        dir_to_list = path_frontier.popleft()
-        sock.sendall(bytes("DIRLIST " + dir_to_list + "\r\n", "utf-8"))
+    request_thread = threading.Thread(target=manage_requests)
+    request_thread.start()
 
-        received = str(sock.recv(1024), "utf-8")
-        print("received: " + received)
-        received_lines = received.split("\r\n")
-        for i in range(1, len(received_lines)-1):
-            received_line = received_lines[i]
-            if (names_in_path[-1] == "/"): #we found a directory
-                path_frontier.append(dir_to_list+received_line)
-            else: #we found a file
-                files_found += 1
-                print('{:,}'.format(files_found) + " file(s) found so far.")
+    response_thread = threading.Thread(target=manage_responses)
+    response_thread.start()
 
-print('{:,}'.format(files_found) + " file(s) found.")
+    files_found = manage_exploration()
+    print('{:,}'.format(files_found) + " file(s) found.")
