461e320ef0c77b9fa200a3a4dd7ec1966d319d2c
Henry Solberg <milkshakeiii@gmail.com>
2021-05-23 17:49:10 -0700

    clean-up

--- interview/server.py
+++ interview/server.py
@@ -1,7 +1,6 @@
 import select
 import socket
 import socketserver
-import time
 
 from collections import deque
 
@@ -24,21 +23,23 @@ class ChallengeTCPHandler(socketserver.BaseRequestHandler):
 
     leftover_received_data = ""
     leftover_unsent_data = bytes()
+    disconnect = False
 
-    def receive_requests(self):
+    def receive_requests(self) -> list:
         received_data = self.request.recv(1024)
-        #print(received_data)
-        #time.sleep(1)
-        last_communication = time.time()
         decoded_data = received_data.decode("utf-8")
         completed_data = self.leftover_received_data + decoded_data
         data_parts = completed_data.split(f"\r\n")
         self.leftover_received_data = data_parts[-1]
         return data_parts[:-1]
 
-    def respond_to_request(self, request):
+    def respond_to_request(self, request: str):
         request_parts = request.split()
         if request_parts[0] == f"DIRLIST":
+            if len(request_parts) > 2:
+                self.error_and_close("Invalid syntax, expected DIRLIST dirname/")
+            if request_parts[1][-1] != "/":
+                self.error_and_close("Directory must start with a '/' character.")
             names_listed = simulated_dirlist(request_parts[1])
             response = f"BEGIN\r\n"
             for name in names_listed:
@@ -47,33 +48,39 @@ class ChallengeTCPHandler(socketserver.BaseRequestHandler):
             send_me = self.leftover_unsent_data + bytes(response, "utf-8")
             sent = self.request.send(send_me)
             self.leftover_unsent_data = send_me[sent:]
-            
+        else:
+            self.error_and_close("Unrecognized request: " + request_parts[0])
+
+    def error_and_close(self, error_message: str):
+        error_bytes = bytes("ERROR " + error_message, "utf-8" + f"\r\n")
+        self.request.sendall(error_bytes)
+        disconnect = True
 
     def handle(self):
-        # self.request is the TCP socket connected to the client
+        print("a new client connected")
+        
         self.request.setblocking(False)
 
-        #prepared_responses = queue.Queue()
         unanswered_requests = deque()
         responses_sent = 0
         
-        client_disconnected = False
-        while not client_disconnected:
+        while not self.disconnect:
+            
             ready_to_read, _, _ = select.select([self.request], [], [], 60)
-            if self.request in ready_to_read:
+            if self.request in ready_to_read: #receive requests
                 requests = self.receive_requests()
                 if len(requests)==0:
-                    client_disconnected = True
+                    self.disconnect = True
                 for request in requests:
                     unanswered_requests.append(request)
 
-            while len(unanswered_requests) > 0:
+            while len(unanswered_requests) > 0: #respond to all requests
                 _, ready_to_send, _ = select.select([], [self.request], [], 60)
-                if self.request in ready_to_send:
+                if self.request in ready_to_send: 
                     request = unanswered_requests.popleft()
                     responses_sent += 1
-                    if (responses_sent%1000)==0:
-                        print(responses_sent)
+                    if (responses_sent%5000)==0:
+                        print(str(responses_sent) + " responses sent to this client.")
                     self.respond_to_request(request)
             
         print("client disconnected, cleaning up")
