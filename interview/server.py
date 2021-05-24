0015266a1b28c3ead96cecc842b9e3a3a4084d25
Henry Solberg <milkshakeiii@gmail.com>
2021-05-23 19:05:38 -0700

    error handling tweaks

--- interview/server.py
+++ interview/server.py
@@ -5,10 +5,25 @@ import socketserver
 from collections import deque
 
 
-def simulated_dirlist(full_path):
+"""
+Returns a list of directory names that would be at the path provided.
+
+Returns an empty list for paths not found in our filesystem.
+"""
+def simulated_dirlist(full_path) -> list:
     full_path = full_path.strip("/")
     names_in_path = full_path.split("/")
-    if len(names_in_path) == 3:
+    for name in names_in_path:
+        if name == "":
+            continue
+        if name[0:4] != "dir_":
+            return []
+        for dir_num in name[5:-1]:
+            if not dir_num.isdigit():
+                return []
+    if len(names_in_path) > 4:
+        return []
+    elif len(names_in_path) == 3:
         return ["file_00", "file_01", "file_02"]
     else:
         return ["dir_%02d/" % (i,) for i in range(100)]
@@ -40,23 +55,26 @@ class ChallengeTCPHandler(socketserver.BaseRequestHandler):
         if request_parts[0] == f"DIRLIST":
             if len(request_parts) > 2:
                 self.error_and_close("Invalid syntax, expected DIRLIST dirname/")
-            if request_parts[1][-1] != "/":
-                self.error_and_close("Directory must start with a '/' character.")
+            if request_parts[1][0] != "/" or request_parts[1][-1] != "/":
+                self.error_and_close("Directory must start and end with a '/' character.")
             names_listed = simulated_dirlist(request_parts[1])
+            if len(names_listed) == 0:
+                self.error_and_close("No directory exists named: " + str(request_parts[1]))
             response = f"BEGIN\r\n"
             for name in names_listed:
                 response += " " + name + f"\r\n"
             response += f"END\r\n"
-            send_me = self.leftover_unsent_data + bytes(response, "utf-8")
-            sent = self.request.send(send_me)
-            self.leftover_unsent_data = send_me[sent:]
+            if (not self.disconnect):
+                send_me = self.leftover_unsent_data + bytes(response, "utf-8")
+                sent = self.request.send(send_me)
+                self.leftover_unsent_data = send_me[sent:]
         else:
             self.error_and_close("Unrecognized request: " + request_parts[0])
 
     def error_and_close(self, error_message: str) -> None:
         error_bytes = bytes("ERROR " + error_message, "utf-8" + f"\r\n")
         self.request.sendall(error_bytes)
-        disconnect = True
+        self.disconnect = True
 
     def handle(self) -> None:
         print("a new client connected")
