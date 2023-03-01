d0e406967f2a3e3c963a22048c56ce36d7f9084a
milkshake <milkshakeiii@gmail.com>
2023-02-28 18:42:13 -0800

    not correctofish

--- other/fish.py
+++ other/fish.py
@@ -18,16 +18,39 @@ def try_out_lure_set(lure_set, try_increment):
     return total_pointwise_distance_for_all_fish
 
 # merely force
-def try_out_lure_set_2(lure_set, try_increment):
-    rz = [lure[0] for lure in lure_set]
-    gz = [lure[0] for lure in lure_set]
-    bz = [lure[0] for lure in lure_set]
-    az = [lure[0] for lure in lure_set]
-    rz, gz, bz, az = map(list, map(sorted, [rz, gz, bz, az]))
-    print(rz)
-    print(gz)
-    print(bz)
-    print(az)
+def try_out_lure_set_2(lure_set, coord_value_max):
+    rz = set([lure[0] for lure in lure_set])
+    gz = set([lure[1] for lure in lure_set])
+    bz = set([lure[2] for lure in lure_set])
+    az = set([lure[3] for lure in lure_set])
+    
+    distance_sums = 0
+    for xz in [rz, gz, bz, az]:
+        xz_min = min(xz)
+        xz_max = max(xz)
+        xz_up = [float('inf')]*coord_value_max
+        xz_down = [float('inf')]*coord_value_max
+        up_step = xz_min
+        distance = 0
+        while (up_step <= coord_value_max):
+            xz_up[up_step-1] = distance
+            up_step += 1
+            distance += 1
+            if (up_step in xz):
+                distance = 0
+        down_step = xz_max
+        distance = 0
+        while (down_step >= 1):
+            xz_down[down_step-1] = distance
+            down_step -= 1
+            distance += 1
+            if (down_step in xz):
+                distance = 0
+        distances = [min(step_distances) for step_distances in zip(xz_up, xz_down)]
+        print(distances)
+        distance_sums += sum(distances)*coord_value_max**3
+    return distance_sums
+
 
 def compute_pointwise_distance(lure, r, g, b, a):
     distance = 0
